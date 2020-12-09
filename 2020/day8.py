3be0727ef1bb1f715aaabb4e6af864e95d51d256
Ed Manlove <emanlove@verizon.net>
2020-12-08 20:45:20 -0500

    Solution for Part Two of Day 8, 2020

--- 2020/day8.py
+++ 2020/day8.py
@@ -1,4 +1,4 @@
-import sys
+import sys,copy
 
 def read_boot_code(filename):
     with open(filename,'r') as fh:
@@ -9,17 +9,20 @@ def read_boot_code(filename):
 def _operation(line,program):
     operation,argument = program[line].split()
     return operation
-    
-if __name__ == "__main__":
-    file = sys.argv[1]
-    boot_code = read_boot_code(file)
 
+def replaceOperation(operation,line,program):
+    alt_program = copy.deepcopy(program)
+    old_operation = _operation(line,alt_program)
+    alt_program[line] = operation + alt_program[line][3:]
+    return alt_program
+
+def executeCode(code):
     accumulator = 0
-    visited_line = [0] * len(boot_code)
+    visited_line = [0] * len(code)
 
     ipntr = 0
     while True:
-        if ipntr == len(boot_code):
+        if ipntr == len(code):
             print(f"COMPLETED: Boot code completed execution!  accumulator:{accumulator}")
             break
         if visited_line[ipntr]:
@@ -27,7 +30,7 @@ if __name__ == "__main__":
             break
 
         visited_line[ipntr] = 1
-        operation,argument = boot_code[ipntr].split()
+        operation,argument = code[ipntr].split()
 
         if operation=='nop':
             ipntr += 1
@@ -36,12 +39,31 @@ if __name__ == "__main__":
             ipntr += 1
         elif operation=='jmp':
             ipntr += int(argument)
-            if ipntr > len(boot_code) or ipntr < 0:
+            if ipntr > len(code) or ipntr < 0:
                 print(f"ERROR: jmp operation out of bounds - {ipntr-int(argument)}->{ipntr}")
                 break
         else:
             print(f"ERROR: Unknown operation - {ipntr}:{operation}")
             break
 
+    return accumulator, visited_line
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    boot_code = read_boot_code(file)
+
+    print("\nPart One:")
+    acummulator,visited_line = executeCode(boot_code)
+
+    print("\nPart Two:")
     nop_before_infinite_loop = [line for line,visited in enumerate(visited_line) if _operation(line,boot_code)=='nop']
+
+    for nop_line in nop_before_infinite_loop:
+        modified_boot_code = replaceOperation('jmp',nop_line,boot_code)
+        acummulator,visited_line = executeCode(modified_boot_code)
+
     jmp_before_infinite_loop = [line for line,visited in enumerate(visited_line) if _operation(line,boot_code)=='jmp']
+
+    for jmp_line in jmp_before_infinite_loop:
+        modified_boot_code = replaceOperation('nop',jmp_line,boot_code)
+        acummulator,visited_line = executeCode(modified_boot_code)

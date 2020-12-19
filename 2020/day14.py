c11c18f57e51d6517fc3b1df18c78c233fe6f15e
Ed Manlove <emanlove@verizon.net>
2020-12-18 22:31:15 -0500

    Didn't realize the input file could have multiple masks ...
    So updated script to deal with that. Also fixed nasty bug with regular expression
    grabbing out the memory address.

--- 2020/day14.py
+++ 2020/day14.py
@@ -4,21 +4,7 @@ def read_init_program(filename):
     with open(filename,'r') as fh:
         program = [line.rstrip('\n') for line in fh]
 
-    maskStr = program[0]
-    _,mask = maskStr.split(' = ')
-    ones=[indx for indx,bit in enumerate(mask[::-1]) if bit=='1']
-    zeros=[indx for indx,bit in enumerate(mask[::-1]) if bit=='0']
-    
-    instructions = []
-    for instr in program[1:]:
-        addrStr,valStr=instr.split(' = ')
-        matches = re.findall(r'[(\d*)]',addrStr)
-        addr = int(matches[0])
-        #val = int(valStr,base=2)
-        val = int(valStr)
-        instructions.append((addr,val))
-
-    return ones,zeros,instructions
+    return program
 
 # `setbit` and `clear_bit` are from RealPython
 # https://realpython.com/python-bitwise-operators/#bitmasks
@@ -29,26 +15,48 @@ def set_bit(value, bit_index):
 def clear_bit(value, bit_index):
     return value & ~(1 << bit_index)
 
+def isBitMaskInstruction(instruction):
+    instr,val = instruction.split(' = ')
+    return True if instr=='mask' else False
+
+def updateMask(instruction):
+    _,mask = instruction.split(' = ')
+    ones=[indx for indx,bit in enumerate(mask[::-1]) if bit=='1']
+    zeros=[indx for indx,bit in enumerate(mask[::-1]) if bit=='0']
+
+    return ones,zeros
+
+def parseMemoryInstruction(instruction):
+    addrStr,valStr=instruction.split(' = ')
+    matches = re.findall(r'\[(\d*)\]',addrStr)
+    addr = int(matches[0])
+    val = int(valStr)
+    return addr,val
+
 if __name__ == "__main__":
     file = sys.argv[1]
 
-    ones,zeros,instructions = read_init_program(file)
+    program = read_init_program(file)
 
+    ones=[]
+    zeros=[]
     memory = {}
-    for instr in instructions:
-        value = int(instr[1])
-        print(f"{value:036b}")
-        for one in ones:
-            value = set_bit(value,one)
-            print(f"{value:036b}")
-        for zero in zeros:
-            value = clear_bit(value,zero)
-            print(f"{value:036b}")
+    for line in program:
+        if isBitMaskInstruction(line):
+            ones,zeros = updateMask(line)
+        else:
+            address,value = parseMemoryInstruction(line)
+            #print(f"{value:036b}")
+            for one in ones:
+                value = set_bit(value,one)
+                #print(f"{value:036b}")
+            for zero in zeros:
+                value = clear_bit(value,zero)
+                #print(f"{value:036b}")
 
-        addr = instr[0]
-
-        print(f"{addr}:{value}")
-        memory[addr] = value
+ 
+            #print(f"{address}:{value}")
+            memory[address] = value
 
     mem_total = sum(memory[a] for a in memory)
     print(f"The total in the memory is {mem_total}")

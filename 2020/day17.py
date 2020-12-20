ee3dae3f151c0a1f2d474fae4b82247a93638b47
Ed Manlove <emanlove@verizon.net>
2020-12-20 18:58:55 -0500

    (Not efficient at all) Solution to Part Two Day 17, 2020
    I am checking all of the space within the -1/+1 limits of each dimension which is excessive.
    First thought is that I could limit the search area to the neighboring space of all active elements
    instead to reduce the amount of checking I am doing.

--- 2020/day17.py
+++ 2020/day17.py
@@ -8,19 +8,36 @@ def read_active_cubes(filename):
     for ycord,yaxis in enumerate(cube_space):
         for xcord,cube in enumerate(yaxis):
             if cube == '#':
-                active_cubes.append((xcord,ycord,0))
+                active_cubes.append((xcord,ycord,0,0))
                 
     return active_cubes
 
 def get_space_limits(cubes):
     nDimensions = [dim for cube in cubes for dim in cube]
-    xDim = nDimensions[::3]
-    yDim = nDimensions[1::3]
-    zDim = nDimensions[2::3]
-    limits =( (min(xDim),max(xDim)), (min(yDim),max(yDim)), (min(zDim),max(zDim)) )
+    xDim = nDimensions[::4]
+    yDim = nDimensions[1::4]
+    zDim = nDimensions[2::4]
+    wDim = nDimensions[2::4]
+    limits =( (min(xDim),max(xDim)), (min(yDim),max(yDim)), (min(zDim),max(zDim)), (min(wDim),max(wDim)) )
 
     return limits
 
+def get_4D_neighbors(cube):
+    neighbors = []
+    xD,yD,zD,wD = cube
+
+    for x in range(xD-1,xD+2):
+        for y in range(yD-1,yD+2):
+            for z in range(zD-1,zD+2):
+                for w in range(wD-1,wD+2):
+                    neighbors += [(x,y,z,w)]
+
+    # pop out self
+    this = neighbors.index(cube)
+    neighbors.pop(this)
+
+    return neighbors
+    
 def get_neighbors(cube):
     neighbors = []
     x,y,z = cube
@@ -47,24 +64,24 @@ if __name__ == "__main__":
 
     for cycle in range(cycles):
         next_active=[]
-        #limits = get_space_limits(active_cubes)
-        (xmin,xmax),(ymin,ymax),(zmin,zmax)  = get_space_limits(active_cubes)
+        (xmin,xmax),(ymin,ymax),(zmin,zmax),(wmin,wmax)  = get_space_limits(active_cubes)
         
         for x in range(xmin-1,xmax+2):
             for y in range(ymin-1,ymax+2):
                 for z in range(zmin-1,zmax+2):
-                    here = (x,y,z)
+                    for w in range(wmin-1,wmax+2):
+                        here = (x,y,z,w)
 
-                    if here in active_cubes:
-                        neighbors = get_neighbors(here)
-                        numActiveNeighbors = sum(1 for neighbor in neighbors if neighbor in active_cubes)
-                        if numActiveNeighbors==2 or numActiveNeighbors==3:
-                            next_active.append(here)
-                    else:
-                        neighbors = get_neighbors(here)
-                        numActiveNeighbors = sum(1 for neighbor in neighbors if neighbor in active_cubes)
-                        if numActiveNeighbors==3:
-                            next_active.append(here)
+                        if here in active_cubes:
+                            neighbors = get_4D_neighbors(here)
+                            numActiveNeighbors = sum(1 for neighbor in neighbors if neighbor in active_cubes)
+                            if numActiveNeighbors==2 or numActiveNeighbors==3:
+                                next_active.append(here)
+                        else:
+                            neighbors = get_4D_neighbors(here)
+                            numActiveNeighbors = sum(1 for neighbor in neighbors if neighbor in active_cubes)
+                            if numActiveNeighbors==3:
+                                next_active.append(here)
 
         active_cubes = next_active
         #print(f"The number of active cubes after {cycle} cycles is {len(active_cubes)}")

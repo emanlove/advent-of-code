1a4bc0d7badd726ec009aa64f59942603dcc6e65
Ed Manlove <emanlove@verizon.net>
2022-12-07 12:57:16 -0500

    Removed Day 6 code

--- 2022/day7.py
+++ 2022/day7.py
@@ -1,13 +1,5 @@
 import sys
 
-def read_datastream(filename):
-    with open(filename,'r') as fh:
-        ds_str = fh.readline().rstrip('\n')
-
-    ds = [c for c in ds_str]
-
-    return ds
-
 def read_multiline(filename):
     with open(filename,'r') as fh:
         lines = [line.rstrip('\n') for line in fh]
@@ -111,57 +103,6 @@ def build_nav_filesystem(code):
                 fs[pwd]['files'].append( (filesize,filename) )
     
     return fs
-
-def find_start_of_packet_marker(ds,size):
-    """
-    There is an interesting "flaw" in the following code .. can you spot it?
-
-    I loop through the entire datastream and look from the current loop index to the index
-    plus the size of the marker. This means that if the marker is not found as the loop
-    approaches the end of the datastream my indexing is looking out past the length of the
-    datastream. Interesting (and I didn't know this nor expect it) but Python did not throw
-    an error on slicing a list outside the bounds of that list. For example, we can setup a
-    list of characters (I know this sequence breaks the puzzle but having unique chars in
-    the datastream helps to demonstrate which characters are at which index),
-
-    >>> datastream = 'abcdefghijklmnopqrst' 
-    >>> len(datastream)
-    20
-    >>> marker_size = 4
-    >>> index = 5
-    >>> datastream[index:index+marker_size] 
-    'fghi'
-    >>> index = 16
-    >>> datastream[index:index+marker_size]
-    'qrst'
-    
-    If we start near the end and our stopping index is out of the range
-
-    >>> index = 18
-    >>> datastream[index:index+marker_size]
-    'st'
-    
-    we see we get just that slice within the bounds of the list. Interesting we can slice
-    entirely outside of the list,
-
-    >>> datastream[27:32]                   
-    ''
-    
-    But if we index outside the  we get, what I was expecting, and out of range error,
-
-    >>> datastream[27]    
-    Traceback (most recent call last):
-      File "<stdin>", line 1, in <module>
-    IndexError: string index out of range
-    >>>     
-        
-    Now as this implementation does not break the solution I am going to leave it as is.
-    But I do see the flaw in my logic which could cause problems moving forward.
-    """
-    
-    for indx,_ in enumerate(ds):
-        if len(set(ds[indx:indx+size])) == size:
-            return indx+size   # noting Python is zero based but answer will be one based
 
 # def calculate_dir_size(dir):
 #     for sub_dir in dir['directories']:

a40c26699be796c8e30111584ffc3999c9716fec
Ed Manlove <emanlove@verizon.net>
2022-12-11 09:09:55 -0500

    Various changes and corrections
    - Fixed parsing notes which left out the last monkey
    - Swapped out lstrip for split in parsing notes. Forgot lsplit does not
      remove a preceding string but removes all charaters in that string.
      Thus in the operand it was removing o from 'old'. Don't like the split
      syntax as much here and makes it a bit ugly. Might clean that up
      later.
    - Moved activity from separate list into an "attribute" of the monkey.
      This allowed me to avoid indexing out the monkeys and a iterated
      through them.
    - Fixed some incorrect dictionary names
    - Added display activity

--- 2022/day11.py
+++ 2022/day11.py
@@ -6,29 +6,32 @@ def read_notes(filename):
     with open(filename,'r') as fh:
         notes = [line.rstrip('\n') for line in fh]
 
+    notes.append('')  #added line at end for parsing last monkey in notes
     return notes
 
 def parse_notes(notes):
-    import pdb;pdb.set_trace()
     notes_on_monkeys = [notes[indx-6:indx] for indx,line in enumerate(notes) if line=='']
 
     monkeys = []
     for note in notes_on_monkeys:
         # Extract worry levels
-        worry_levelSTR = note[1].lstrip('  Starting items: ').split(', ')
+        worry_levelSTR = note[1].split('  Starting items: ')[1].split(', ')
         worry_level = [int(_) for _ in worry_levelSTR]
+        print(f"{worry_level}")
         # Extract Operation
-        oper = note[2].lstrip('  Operation: new = ')
+        oper = note[2].split('  Operation: new = ')[1]
         operand,operator,modifier = oper.split(' ')
         operation = {'operand':operand,'operator':operator,'modifier':modifier}
         # Extract Test
-        test = int(note[3].lstrip('  Test: divisible by '))
-        if_true = int(note[4].lstrip('    If true: throw to monkey '))
-        if_false = int(note[5].lstrip('    If false: throw to monkey '))
+        test = int(note[3].split('  Test: divisible by ')[1])
+        if_true = int(note[4].split('    If true: throw to monkey ')[1])
+        if_false = int(note[5].split('    If false: throw to monkey ')[1])
+        print(f"{if_true}  {if_false}")
 
-        attr = {'worry_levels':worry_level, 'operation':operation, 'test':test, 'if_true':if_true, 'if_false':if_false}
+        attr = {'worry_levels':worry_level, 'operation':operation, 'test':test, 'if_true':if_true, 'if_false':if_false, 'activity':0}
         monkeys.append(attr)
     
+    print(f"{len(monkeys)}")
     return monkeys
 
 def inspect_item(level,oper):
@@ -44,9 +47,9 @@ def inspect_item(level,oper):
     else:
         oper['modifier'] = int(oper['modifier'])
 
-    if oper['operater'] == '+':
+    if oper['operator'] == '+':
         new_level = oper['operand'] + oper['modifier']
-    elif oper['operater'] == '*':
+    elif oper['operator'] == '*':
         new_level = oper['operand'] * oper['modifier']
     else:
         print(f"WARNING: Unkown operator!")
@@ -62,27 +65,35 @@ def throw_item(test):
 # monkey throws item
 
 def play_keep_away(monkeys):
-    monkey_activity = [0 for _ in range(len(monkeys))]
 
     for round in range(TOTAL_NUM_ROUNDS):
         for monkey in monkeys:
-            for item in monkey['worry_level']:
+            for item in monkey['worry_levels']:
                 # inspects an item
                 inspected_level = inspect_item(item, monkey['operation'])
                 # factor in my relief
                 relief_level = inspected_level//3
                 # monkey tests your worry level and throws item
                 if relief_level % monkey['test']:
-                    monkeys[monkey['if_true']]['worry_level'] = relief_level
+                    monkeys[monkey['if_true']]['worry_levels'].append(relief_level)
                 else:
-                    monkeys[monkey['if_false']]['worry_level'] = relief_level            
-            monkey['worry_level'] = []
+                    monkeys[monkey['if_false']]['worry_levels'].append(relief_level)
+            monkey['activity'] += len(monkey['worry_levels'])
+            monkey['worry_levels'] = []
+
+    return monkeys
+
+def display_monkey_total_activity(monkeys):
+    for indx,monkey in enumerate(monkeys):
+        print(f"Monkey {indx} inspected items {monkey['activity']} times.")
 
 if __name__ == "__main__":
     file = sys.argv[1]
 
     notes = read_notes(file)
     notes_about_monkeys = parse_notes(notes)
+    monkeys = play_keep_away(notes_about_monkeys)
+    display_monkey_total_activity(monkeys)
 
     # print(f"The sum of the signal strengths is {total_strengths}")
     # part1_ans = total_strengths

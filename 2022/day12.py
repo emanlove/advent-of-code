0f531d5b05e7887e00d185f8b439ee87adb28d42
Ed Manlove <emanlove@verizon.net>
2022-12-19 16:33:30 -0500

    Working on a solution for Day 12: Part 1
    - Added a function to show all points where one can step up from one
      letter to the next
    - Added a map for where one can both enter or exit a square from its
      neighbors
    - Added some notes and thoughts about solution

--- 2022/day12.py
+++ 2022/day12.py
@@ -1,3 +1,13 @@
+"""
+Some thoughts ...
+
+- Every point if used will only be used once. That is if it were used twice, thus the trail
+  would be circling back upon itself, and thus NOT the shortest route.
+- Maximum path length is NRows*nCols and the minimum is 25.
+- There are step up points where one can olny step up to the next letter. In both th test
+  data and in the problem data there are some limits here.
+"""
+
 import sys
 
 def read_map(filename,cleanup=True):
@@ -20,7 +30,8 @@ def read_map(filename,cleanup=True):
 def create_movement_maps(map,nCols,nRows):
     # left up right down
     neighbors = []
-    allowed_pathway = []
+    allowed_exit_pathway = []
+    allowed_entr_pathway = []
 
     for indx,point in enumerate(map):
         col = indx%nCols
@@ -36,21 +47,45 @@ def create_movement_maps(map,nCols,nRows):
 
         neighbors.append([left_neighbor, up_neighbor, right_neighbor, down_neighbor])
 
-        left_pathway  = False if ( (left_neighbor is None)  or (ord(map[left_neighbor])-ord(point)>1)) else True
-        up_pathway    = False if ( (up_neighbor is None)    or (ord(map[up_neighbor])-ord(point)>1)) else True
-        right_pathway = False if ( (right_neighbor is None) or (ord(map[right_neighbor])-ord(point)>1)) else True
-        down_pathway  = False if ( (down_neighbor is None)  or (ord(map[down_neighbor])-ord(point)>1)) else True
+        left_exit_pathway  = False if ( (left_neighbor is None)  or (ord(map[left_neighbor])-ord(point)>1)) else True
+        up_exit_pathway    = False if ( (up_neighbor is None)    or (ord(map[up_neighbor])-ord(point)>1)) else True
+        right_exit_pathway = False if ( (right_neighbor is None) or (ord(map[right_neighbor])-ord(point)>1)) else True
+        down_exit_pathway  = False if ( (down_neighbor is None)  or (ord(map[down_neighbor])-ord(point)>1)) else True
 
-        allowed_pathway.append([left_pathway, up_pathway, right_pathway, down_pathway])
+        allowed_exit_pathway.append([left_exit_pathway, up_exit_pathway, right_exit_pathway, down_exit_pathway])
 
-    return neighbors,allowed_pathway
+        left_entr_pathway  = False if ( (left_neighbor is None)  or (ord(point)-ord(map[left_neighbor])<1)) else True
+        up_entr_pathway    = False if ( (up_neighbor is None)    or (ord(point)-ord(map[up_neighbor])<1)) else True
+        right_entr_pathway = False if ( (right_neighbor is None) or (ord(point)-ord(map[right_neighbor])<1)) else True
+        down_entr_pathway  = False if ( (down_neighbor is None)  or (ord(point)-ord(map[down_neighbor])<1)) else True
+
+        allowed_entr_pathway.append([left_entr_pathway, up_entr_pathway, right_entr_pathway, down_entr_pathway])
+
+    return neighbors,allowed_exit_pathway,allowed_entr_pathway
+
+def step_up_spots(map,neighbors,frm,to):
+    step_up_points = []
+    for indx,point in enumerate(map):
+        if point != to:
+            continue
+        for n in neighbors[indx]:
+            if (n is not None) and map[n]==frm:
+                step_up_points.append((indx,n))
+
+    return step_up_points
 
 if __name__ == "__main__":
     file = sys.argv[1]
 
     map,nCols,nRows,Sindx,Eindx  = read_map(file)
-    neighbors,pathways = create_movement_maps(map,nCols,nRows)
+    neighbors,exits,entries = create_movement_maps(map,nCols,nRows)
 
+    letters = [chr(o) for o in range(ord('a'),ord('a')+26)]
+    steps = list(zip(letters[:-1],letters[1:]))
+    for step in steps[2:]:
+        stepping_points = step_up_spots(map,neighbors,step[0],step[1])
+        print(f"steps {step}: {stepping_points}")
+    
     # print(f"The level of monkey business is {monkey_business_level}")
     # part1_ans = monkey_business_level
 

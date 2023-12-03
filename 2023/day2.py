59c63cd50a3799a595a79f58511d308ec08daad6
Ed Manlove <emanlove@verizon.net>
2023-12-02 20:41:41 -0500

    Solution for Day 2 part 2

--- 2023/day2.py
+++ 2023/day2.py
@@ -1,4 +1,5 @@
 import sys
+from functools import reduce
 
 LIMITS = {'red': 12, 'green': 13, 'blue': 14}
 
@@ -7,6 +8,7 @@ def read_games(filename):
         lines = [line.rstrip('\n') for line in fh]
     
     valid_games = []
+    powers = []
     for line in lines:
         g, r = line.split(': ')
         _,game = g.split('Game ')
@@ -22,7 +24,11 @@ def read_games(filename):
         if is_game_valid(r):
             valid_games.append(game)
 
+        power = minimum(r)
+        powers.append(power)
+    
     print(sum(valid_games))
+    print(f"the sum of the power of these sets is {sum(powers)}")
 
 def is_game_valid(r):
     reveals = r.split('; ')
@@ -34,6 +40,19 @@ def is_game_valid(r):
                 return False
     return True
 
+def minimum(r):
+    cubes = {'red': [], 'green': [], 'blue': []}
+    reveals = r.split('; ')
+    powers = []
+    for reveal in reveals:
+        colors = reveal.split(', ')
+        for c in colors:
+            count, color = c.split(' ')
+            cubes[color].append(int(count))
+    fewest_cubes = [max(cubes[c])for c in cubes]
+    # print(f"{fewest_cubes}")
+    power = reduce(lambda x, y: x*y, fewest_cubes)
+    return power
 
 if __name__ == "__main__":
     file = sys.argv[1]

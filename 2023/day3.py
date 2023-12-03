99512abd8ec83a2d3d2c45274cf9210945c1f7bd
Ed Manlove <emanlove@verizon.net>
2023-12-03 13:21:37 -0500

    Solution for Part 1 Day 3
    Reworked the way I parsed out the numbers and symbols. The previous
    method had a few issues. One if a symbol came right before or after a
    number the split on `.` would not have separated the number from the
    symbol. Also as I did this on a flattened schematic is a number was on
    the end of line and one on the beginning of the next line those would
    have been ccombined. Finally if two symbol were next to each other the
    split would have also counted this as one symbol. So switched to a more
    character by character parsing.

--- 2023/day3.py
+++ 2023/day3.py
@@ -1,42 +1,89 @@
 import sys
 
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
 def read_schematic(filename):
     with open(filename,'r') as fh:
         lines = [line.rstrip('\n') for line in fh]
     
     nCols = len(lines[0])
     #rows = len(lines)
-
-    schematic = ''.join(lines)
-    objs = schematic.split('.')
+    
+    flattened_schematic = ''.join(lines)
 
     part_numbers = {}
     symbols = {}
     any_symbol = []
-    pos = 0
-    for obj in objs:
-        if is_number(obj):
-            # ? can we have duplicate part numbers .. assuming yes
+
+    for row,line in enumerate(lines):
+        obj = ''
+        pos = None
+        for col,char in enumerate(line):
+            if char in DIGITS:
+                if not obj:
+                    pos = col + (row*nCols)
+                obj += char
+            elif char=='.':
+                if obj:  # close out number if there
+                    if obj not in part_numbers:
+                        part_numbers[obj] = []                    
+                    part_numbers[obj].append(pos)
+                    obj = ''
+                    pos = None
+            else:  # symbol
+                if obj:  # close out number if there
+                    if obj not in part_numbers:
+                        part_numbers[obj] = []                    
+                    part_numbers[obj].append(pos)
+                    obj = ''
+                    pos = None
+                pos = col + (row*nCols)
+                # record individual symbols in case if needed in part 2
+                if char not in symbols:
+                    symbols[char] = []
+                symbols[char].append(pos)
+                any_symbol.append(pos)
+                pos = None
+        # reached end of line and need to ..
+        if obj:  # .. close out number if there
+            print(f"{obj}")
             if obj not in part_numbers:
-                part_numbers[obj] = []
+                part_numbers[obj] = []                    
             part_numbers[obj].append(pos)
-            # schematic.find(obj)            
-            pos += len(obj)
-        elif obj=='':            
-            pos += 1
-        else:
-            if obj not in symbols:
-                symbols[obj] = []
-            symbols[obj].append(pos)
-            any_symbol.append(pos)
-            pos += 1  # assuming symbols are single characters and any adjacent symbols are sepearet symbols
+
+    # print(f"{flattened_schematic}")
+    # import pdb;pdb.set_trace()
+
+    # schematic = ''.join(lines)
+    # objs = schematic.split('.')
+
+    # part_numbers = {}
+    # symbols = {}
+    # any_symbol = []
+    # pos = 0
+    # for obj in objs:
+    #     if is_number(obj):
+    #         # ? can we have duplicate part numbers .. assuming yes
+    #         if obj not in part_numbers:
+    #             part_numbers[obj] = []
+    #         part_numbers[obj].append(pos)
+    #         # schematic.find(obj)            
+    #         pos += len(obj)
+    #     elif obj=='':            
+    #         pos += 1
+    #     else:
+    #         if obj not in symbols:
+    #             symbols[obj] = []
+    #         symbols[obj].append(pos)
+    #         any_symbol.append(pos)
+    #         pos += 1  # assuming symbols are single characters and any adjacent symbols are sepearet symbols
 
     symbol_chars = symbols.keys()
 
     valid_parts = []
     for part in part_numbers:
         for duplicate_start in part_numbers[part]:
-            possible_part = valid_part_number_check(part,duplicate_start,schematic,nCols,symbol_chars)
+            possible_part = valid_part_number_check(part,duplicate_start,flattened_schematic,nCols,symbol_chars)
 
             if possible_part is not None:
                 valid_parts.append(possible_part)

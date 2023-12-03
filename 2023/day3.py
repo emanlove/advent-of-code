ecd82db186a96bddf097c37f813a55bd67290eac
Ed Manlove <emanlove@verizon.net>
2023-12-03 11:51:04 -0500

    Added logic to find which number sets have an adjacent symbol
    Gong to break this out into a function and still need to reolve issue
    with symbols at start or end of number set.

--- 2023/day3.py
+++ 2023/day3.py
@@ -4,10 +4,11 @@ def read_schematic(filename):
     with open(filename,'r') as fh:
         lines = [line.rstrip('\n') for line in fh]
     
-    cols = len(lines[0])
-    rows = len(lines)
+    nCols = len(lines[0])
+    #rows = len(lines)
 
     schematic = ''.join(lines)
+    schematic_range = range(len())
     objs = schematic.split('.')
 
     part_numbers = {}
@@ -16,7 +17,7 @@ def read_schematic(filename):
     pos = 0
     for obj in objs:
         if is_number(obj):
-            # ? can we have duplicate part numbers
+            # ? can we have duplicate part numbers .. assuming yes
             if obj not in part_numbers:
                 part_numbers[obj] = []
             part_numbers[obj].append(pos)
@@ -29,12 +30,24 @@ def read_schematic(filename):
                 symbols[obj] = []
             symbols[obj].append(pos)
             any_symbol.append(pos)
-            pos += 1  # assuming symbls are single characters and any adjacent symbols are sepearet symbols
+            pos += 1  # assuming symbols are single characters and any adjacent symbols are sepearet symbols
 
+    symbol_chars = symbols.keys()
 
-    import pdb;pdb.set_trace()
+    for part in part_numbers:
+        for duplicate_start in part_numbers[part]:
+            for index,digit in enumerate(part):
+                pos = duplicate_start + index
+                adjacent = [pos-nCols-1, pos-nCols, pos-nCols+1,
+                             pos-1,                  pos+1,
+                             pos+nCols-1, pos+nCols, pos+nCols+1]
 
-    print(f"the sum of the power of these sets is ")
+                for point in adjacent:
+                    if point in schematic_range and schematic[point] in symbol_chars:
+                        # add part number to list of valid part numbers
+                        pass
+
+    print(f"The sum of all of the part numbers in the engine schematic is ")
 
 def is_number(obj):
     try:

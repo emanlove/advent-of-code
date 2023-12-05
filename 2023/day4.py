5ade247394480a8a015561c0785345074c17401e
Ed Manlove <emanlove@verizon.net>
2023-12-04 21:30:04 -0500

    Added pseudo code for one method to count all the cards up
    I know there are a few methods.One which seems the best would be some
    recursive/generator type parse through and count. But admittedly my
    create generator in Python is a bit weak. I am going to ask in a user
    group about this but in the mean time I think this "algorithm" should
    proper count out the total number of cards.

--- 2023/day4.py
+++ 2023/day4.py
@@ -9,6 +9,7 @@ def read_cards(filename):
     
     cards = {}
     points = []
+    winning_cards = {}
     for line in lines:
         c, n = line.split(': ')
         _,card = c.split('Card ')
@@ -20,11 +21,25 @@ def read_cards(filename):
         my_numbers = myNumStr.split(' ')
         my_numbers = [n for n in my_numbers if n!='']
         matching_numbers = set(winning_nums) & set(my_numbers)
-        num_matchs = len(matching_numbers)
+        num_matchs = len(matching_numbers)        
         point_value = 2**(num_matchs-1) if num_matchs else 0
         points.append(point_value)
 
+        # winning_cards[card] = [i for i in range(card+1,card+1+num_matchs)]
+        won_cards[card] = [i for i in range(card+1,card+1+num_matchs)]
+
+        # make deep copy of winning/won_cards into counting_cards
+        # count number of cards remaining in counting_cards (len of keys) and add to total
+        # remove any keys with empty lists
+        # starting with the back of the list
+        #   for any card in values whose list is no longer in the keys add it back in
+        #      or if num_matches for that card is zero add in a self reference (so it will be counted on next round)
+        #      or for any self referencing cards (ie 5 in 5) pop just one each counting round
+        #      or(?) for all cards add it's won_cards to other counting_cards stack poping off that card
+        #  .. repeat
+    
     print(f"The total worth of points for the cards is {sum(points)}")
+    import pdb;pdb.set_trace()
 
 if __name__ == "__main__":
     file = sys.argv[1]

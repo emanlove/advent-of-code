b025ff4cfe95d12ff7a8618c60986e8db2c43212
Ed Manlove <emanlove@verizon.net>
2023-12-04 22:29:17 -0500

    Solution for part 2 day 4
    My first method seemed to have some flaws with updating the list of
    ones to count while counting. So switched method and just said ..
    
    starting with every card ( a list of ones)
    while there was more to count do ..
      add the number of cards to the total then ..
      replace the list of counts with the multiple of each won card
    
    Simple as that :)

--- 2023/day4.py
+++ 2023/day4.py
@@ -1,5 +1,6 @@
 import sys
 from functools import reduce
+from copy import deepcopy
 
 LIMITS = {'red': 12, 'green': 13, 'blue': 14}
 
@@ -9,7 +10,7 @@ def read_cards(filename):
     
     cards = {}
     points = []
-    winning_cards = {}
+    won_cards = {}
     for line in lines:
         c, n = line.split(': ')
         _,card = c.split('Card ')
@@ -28,18 +29,57 @@ def read_cards(filename):
         # winning_cards[card] = [i for i in range(card+1,card+1+num_matchs)]
         won_cards[card] = [i for i in range(card+1,card+1+num_matchs)]
 
-        # make deep copy of winning/won_cards into counting_cards
-        # count number of cards remaining in counting_cards (len of keys) and add to total
-        # remove any keys with empty lists
-        # starting with the back of the list
-        #   for any card in values whose list is no longer in the keys add it back in
-        #      or if num_matches for that card is zero add in a self reference (so it will be counted on next round)
-        #      or for any self referencing cards (ie 5 in 5) pop just one each counting round
-        #      or(?) for all cards add it's won_cards to other counting_cards stack poping off that card
-        #  .. repeat
-    
+
     print(f"The total worth of points for the cards is {sum(points)}")
-    import pdb;pdb.set_trace()
+
+        # # make deep copy of winning/won_cards into counting_cards
+        # counting_cards = deepcopy(won_cards)
+        # total_cards = 0
+        # while counting_cards:
+        #     # count number of cards remaining in counting_cards (len of keys) and add to total
+        #     total_cards += len(counting_cards.keys())
+        #     # remove any keys with empty lists
+        #     for card in counting_cards:
+        #         if not counting_cards[card]:
+        #             counting_cards.pop(card)
+            
+
+        # # make deep copy of winning/won_cards into counting_cards
+        # # count number of cards remaining in counting_cards (len of keys) and add to total
+        # # remove any keys with empty lists
+        # # starting with the back of the list
+        # #   for any card in values whose list is no longer in the keys add it back in
+        # #      or if num_matches for that card is zero add in a self reference (so it will be counted on next round)
+        # #      or for any self referencing cards (ie 5 in 5) pop just one each counting round
+        # #      or(?) for all cards add it's won_cards to other counting_cards stack poping off that card
+        # #  .. repeat
+    
+
+    # make to_count list of len(lines) of ones
+    num_cards = len(lines)
+    to_count = [1] * num_cards
+    total_cards = 0
+    # while not all zeros
+    while (any(to_count)):
+        # print(f"{to_count}")
+        # count all non zeros and add to total
+        # addend = sum([1 for _ in to_count if _>0])
+        addend = sum(to_count)
+        total_cards += addend
+        # for each card in to_count make addition list
+        additional = [0] * num_cards
+        for indx,multiple in enumerate(to_count):
+            card_indx = indx+1
+            for add_this_card in won_cards[card_indx]:
+                zero_indx = add_this_card-1
+                additional[zero_indx] += multiple
+            
+        # uncount or simply replace???
+        to_count = additional
+
+    print(f"The total number of scratchcards I ended up with is {total_cards}")
+
+    # import pdb;pdb.set_trace()
 
 if __name__ == "__main__":
     file = sys.argv[1]

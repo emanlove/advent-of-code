edcb0f31d9efb4c796720400dcef61237eccab90
Ed Manlove <emanlove@verizon.net>
2023-12-08 20:14:47 -0500

    Solution for Day 7 Part 2
    Could use a bit of cleaning up but working solution

--- 2023/day7.py
+++ 2023/day7.py
@@ -1,7 +1,9 @@
 import sys
 from collections import Counter
+from copy import deepcopy
 
 ORDERED = 'AKQJT98765432'
+ORDERED_JOKERS = 'AKQT98765432J'
 
 def read_CamelCards(filename):
     with open(filename,'r') as fh:
@@ -35,8 +37,57 @@ def determine_strength(hand):
     else:
         return 'HIGH'   # 'High card'
 
+def determine_strength_with_jokers(hand):
+    cards_in_hand = [card for card in hand]
+
+    counting_cards = Counter(cards_in_hand)
+    counting_cards_without_jokers = deepcopy(counting_cards)
+    if 'J' in counting_cards:
+        joker_count = counting_cards_without_jokers.pop('J')
+    else:
+        joker_count = 0
+    
+    counts = counting_cards_without_jokers.values()
+    max_count = max(counting_cards_without_jokers.values()) if counting_cards_without_jokers else 0
+
+    if max_count+joker_count == 5:
+        return 'FIVE'
+
+    if max_count+joker_count == 4:
+        return 'FOUR'
+
+    if joker_count == 0:
+        if 5 in counts:
+            return 'FIVE'   # 'Five of a kind'
+        elif 4 in counts:
+            return 'FOUR'   # 'Four of a kind'
+        elif 3 in counts and 2 in counts:
+            return 'FULL'   # 'Full house'
+        elif 3 in counts:
+            return 'THREE'  # 'Three of a kind'
+        elif 2 in counts and Counter(counts)[2] == 2:
+            return 'TWO'    # 'Two pair'
+        elif 2 in counts:
+            return 'ONE'    # 'One pair'
+        else:
+            return 'HIGH'   # 'High card'
+    else:
+        if max_count+joker_count == 5:
+            return 'FIVE'
+        elif max_count+joker_count == 4:
+            return 'FOUR'
+        elif Counter(counts)[2] == 2 and joker_count == 1: # we could asume joker_count==1 is true by logic
+            return 'FULL'
+        elif  max_count+joker_count == 3:
+            return 'THREE'
+        # would never get two pairs with joker as that scenario the joker would make three of kind
+        elif max_count+joker_count == 2:
+            return 'ONE'
+        else:
+            raise ValueError('Should never get this')
+
 def sort_cards(cards):
-    return sorted(cards, key=lambda word: [ORDERED.index(c) for c in word])
+    return sorted(cards, key=lambda word: [ORDERED_JOKERS.index(c) for c in word])
 
 if __name__ == "__main__":
     file = sys.argv[1]
@@ -47,7 +98,8 @@ if __name__ == "__main__":
     types_of_cards = {t:[] for t in types}
 
     for hand in cc:
-        strength = determine_strength(hand)
+        # strength = determine_strength(hand)
+        strength = determine_strength_with_jokers(hand)
         types_of_cards[strength].append(hand)
         print(f"{hand}: {strength}")
     

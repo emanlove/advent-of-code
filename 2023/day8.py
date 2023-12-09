6c30b40a246f33073b7bef9017bc28bedd4a31f5
Ed Manlove <emanlove@verizon.net>
2023-12-08 21:11:05 -0500

    Initial attempt at Part 2 of Day 8
    Seems to take forever so rethinking solution. Realized one could find
    how many steps it takes for each A node to get to (possibly?) each Z
    node. Then calcuate something like the least common factor for each.

--- 2023/day8.py
+++ 2023/day8.py
@@ -13,21 +13,47 @@ def read_document(filename):
 
     return lr_instructions, map
 
+# if __name__ == "__main__":
+#     file = sys.argv[1]
+
+#     lr_instructions,map = read_document(file)
+
+#     node = 'AAA'
+#     len_inst = len(lr_instructions)
+#     inst_indx = 0
+#     steps = 0
+#     while node != 'ZZZ':
+#         move_direction = lr_instructions[inst_indx]
+#         node = map[node][move_direction]
+#         steps += 1
+#         inst_indx +=1
+#         if inst_indx == len_inst:
+#             inst_indx = 0
+    
+#     print(f"The number of steps required to reach ZZZ is {steps}")
+
+
+def all_nodes_endswith_Z (nodes):
+    return all([1 if node.endswith('Z') else 0 for node in nodes])
+
 if __name__ == "__main__":
     file = sys.argv[1]
 
     lr_instructions,map = read_document(file)
 
-    node = 'AAA'
+    nodes = [node for node in map if node.endswith('A')]
+
+    print(f"{nodes}")
+    import pdb;pdb.set_trace()
     len_inst = len(lr_instructions)
     inst_indx = 0
     steps = 0
-    while node != 'ZZZ':
+    while not all_nodes_endswith_Z(nodes):
         move_direction = lr_instructions[inst_indx]
-        node = map[node][move_direction]
+        nodes = [map[node][move_direction] for node in nodes]
         steps += 1
         inst_indx +=1
         if inst_indx == len_inst:
             inst_indx = 0
     
-    print(f"The number of steps required to reach ZZZ is {steps}")
+    print(f"The number of steps before I'm only on nodes that end with Z is {steps}")

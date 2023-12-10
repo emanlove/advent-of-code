f64c776460e64c0c91b1f086422c90ae6246d86a
Ed Manlove <emanlove@verizon.net>
2023-12-10 14:39:56 -0500

    Initial solution for Part 2 of Day 10
    See that there is an error here. I believe my check for inside and
    outside should take previous state into account. ?? Or at least I know I
    am missing something here ..

--- 2023/day10.py
+++ 2023/day10.py
@@ -13,11 +13,6 @@ LEADING_NORTH = ['7', '|', 'F', 'S']
 LEADING_EAST  = ['L', '-', 'F', 'S']
 LEADING_WEST  = ['J', '-', '7', 'S']
 LEADING_SOUTH = ['J', '|', 'L', 'S']
-# LEADING_NORTH = {'7':-1, '|':0, 'F':1, 'S':0}
-# LEADING_EAST  = {'L':-1, '-':0, 'F':1, 'S':0}
-# LEADING_WEST  = {'J':-1, '-':0, '7':1, 'S':0}
-# LEADING_SOUTH = {'J':-1, '|':0, 'L':1, 'S':0}
-
 
 # coming_from, going_to
 def read_pipes(filename):
@@ -33,7 +28,6 @@ def read_pipes(filename):
                     'F': [ 1    , nCols]
                   }
 
-
     for indx,line in enumerate(lines):
         if 'S' in line:
             # start = (line.index('S'),indx)
@@ -41,7 +35,6 @@ def read_pipes(filename):
             break
 
     flattened_map = ''.join(lines)
-
 
     starting_connections = find_pipes_leading_out_start(flattened_map, start, nCols)
     next = starting_connections[0]  # arbitrarily choose one
@@ -58,23 +51,22 @@ def read_pipes(filename):
         next = connecting_pipes[0]
         path.append(next)
 
-    # connections = get_connecting_pipes(flattened_map, start, nCols)
-    # next = connections[0]
-    # prev = start
-    # path = [start,next]
-    # while flattened_map[next] != 'S':
-    #     connections = get_connecting_pipes(flattened_map, next, nCols)
-    #     connections.pop(connections.index(prev))
-    #     #sanity check
-    #     if len(connections) != 1:
-    #         raise ValueError(f"At this point connections should have length of 1 but does not. {connections}")
-    #     prev = next
-    #     next = connections[0]
-    #     path.append(next)
-    
     farthest_steps = len(path)//2
     print(f"The number of steps farthest from the starting position is {farthest_steps}")
-    # import pdb;pdb.set_trace()
+
+    num_inside_tiles = 0
+    for row,line in enumerate(lines):
+        inside = False
+        for col,tile in enumerate(line):
+            abs_pos = col + (row*nCols)
+            if abs_pos in path:
+                # toggle outside/inside
+                inside = True if not inside else False
+                # continue
+            elif inside and flattened_map[abs_pos]=='.':
+                num_inside_tiles +=1
+
+    print(f"The number of tiles are enclosed by the loop is {num_inside_tiles}")
 
 def find_pipes_leading_out_start(map, this, nCols):
     connections = []
@@ -105,41 +97,6 @@ def find_pipes_leading_out_start(map, this, nCols):
 
     if len(connections) != 2:
         raise ValueError(f"Number of connection leading out of start should be two but was {len(connections)} instead.")
-    
-    return connections
-
-def get_connecting_pipes(map, this, nCols):
-    connections = []
-    
-    # Check North
-    north_pos = this-nCols
-    north = map[north_pos]
-    if north in LEADING_NORTH:
-        connections.append(north_pos+LEADING_NORTH[north])
-    # elif north == 'S':
-    #     connections.append(north_pos)
-
-
-    # Check East
-    east_pos = this-1
-    east = map[east_pos]
-    if east in LEADING_EAST:
-        connections.append(east_pos + (LEADING_EAST[east]*nCols) )
-
-    # Check West
-    west_pos = this+1
-    west = map[west_pos]
-    if west in LEADING_WEST:
-        connections.append(west_pos + (LEADING_WEST[west]*nCols) )
-    
-    # Check South
-    south_pos = this+nCols
-    south = map[south_pos]
-    if south in LEADING_SOUTH:
-        connections.append(south_pos+LEADING_SOUTH[south])
-
-    if len(connections) != 2:
-        raise ValueError(f"Number of connection leading out of {this} should be two but was {len(connections)} instead.")
     
     return connections
 

b4b170a7148dbbc9e7b28505ef90b62a3ed87ce2
Ed Manlove <emanlove@verizon.net>
2023-12-09 20:29:33 -0500

    Resolved issue for solution of Day 9 part 1
    My check for all zeros was faulty. See now how clearyly that was just
    wrong. And my second attempt was only slighty more bad. Finally did it
    the old fasion way .. still wondering though if there is a more pythonic
    way of checking all zeros??

--- 2023/day9.py
+++ 2023/day9.py
@@ -3,22 +3,33 @@ import sys
 def read_OASIS_report(filename):
     with open(filename,'r') as fh:
         lines = [line.rstrip('\n') for line in fh]
-    
+
+    print(f"Number of lines: {len(lines)}")    
     next_values = []
     for line in lines:
+        # print("--------------------")
         history = [int(item) for item in line.split()]
+        # print(f"{history}")
+        # import pdb;pdb.set_trace()
         last = [history[-1]]
         while not all_zeros(history):
             history = [history[indx+1]-history[indx] for indx,_ in enumerate(history[:-1])]
             # print(f"{history}")
             last.append(history[-1])
-        # print(f"{last}")
+        # print(f"{last}")        
         next_values.append(sum(last))
+        last = []
 
+    print(f"Number of values: {len(next_values)}")
     print(f"The sum of the extrapolated values is {sum(next_values)}")
 
 def all_zeros(dataset):
-    return sum(dataset)==0
+    for item in dataset:
+        if item != 0:
+            return False
+    return True
+    # return all([1 for item in dataset if item==0])
+    # return sum(dataset)==0
 
 if __name__ == "__main__":
     file = sys.argv[1]

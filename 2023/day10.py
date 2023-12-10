97d679a30882dd99e75b7fba8b1248e6890cbdb4
Ed Manlove <emanlove@verizon.net>
2023-12-10 12:26:46 -0500

    Solution for Day 10 Part 1
    This is a bit of a mess. I really didn't want to get rid of my original
    idea. Might clean this up later. But solution is here.

--- 2023/day10.py
+++ 2023/day10.py
@@ -8,17 +8,32 @@ F is a 90-degree bend connecting south and east.
 """
 import sys
 
-# LEADING_NORTH = ['|', '7', '']
-LEADING_NORTH = {'7':-1, '|':0, 'F':1, 'S':0}
-LEADING_EAST  = {'L':-1, '-':0, 'F':1, 'S':0}
-LEADING_WEST  = {'J':-1, '-':0, '7':1, 'S':0}
-LEADING_SOUTH = {'J':-1, '|':0, 'L':1, 'S':0}
 
+LEADING_NORTH = ['7', '|', 'F', 'S']
+LEADING_EAST  = ['L', '-', 'F', 'S']
+LEADING_WEST  = ['J', '-', '7', 'S']
+LEADING_SOUTH = ['J', '|', 'L', 'S']
+# LEADING_NORTH = {'7':-1, '|':0, 'F':1, 'S':0}
+# LEADING_EAST  = {'L':-1, '-':0, 'F':1, 'S':0}
+# LEADING_WEST  = {'J':-1, '-':0, '7':1, 'S':0}
+# LEADING_SOUTH = {'J':-1, '|':0, 'L':1, 'S':0}
+
+
+# coming_from, going_to
 def read_pipes(filename):
     with open(filename,'r') as fh:
         lines = [line.rstrip('\n') for line in fh]
 
     nCols = len(lines[0])
+    connections = { '|': [-nCols, nCols],
+                    '-': [-1    , 1    ],
+                    'L': [-nCols, 1    ],
+                    'J': [-nCols, -1   ],
+                    '7': [-1    , nCols],
+                    'F': [ 1    , nCols]
+                  }
+
+
     for indx,line in enumerate(lines):
         if 'S' in line:
             # start = (line.index('S'),indx)
@@ -27,23 +42,71 @@ def read_pipes(filename):
 
     flattened_map = ''.join(lines)
 
-    connections = get_connecting_pipes(flattened_map, start, nCols)
-    next = connections[0]
+
+    starting_connections = find_pipes_leading_out_start(flattened_map, start, nCols)
+    next = starting_connections[0]  # arbitrarily choose one
     prev = start
     path = [start,next]
     while flattened_map[next] != 'S':
-        connections = get_connecting_pipes(flattened_map, next, nCols)
-        connections.pop(connections.index(prev))
+        move_by = connections[flattened_map[next]]
+        connecting_pipes = [next+step for step in move_by]
+        connecting_pipes.pop(connecting_pipes.index(prev))
         #sanity check
-        if len(connections) != 1:
-            raise ValueError(f"At this point connections should have length of 1 but does not. {connections}")
+        if len(connecting_pipes) != 1:
+            raise ValueError(f"At this point connections should have length of 1 but does not. {connecting_pipes}")
         prev = next
-        next = connections[0]
+        next = connecting_pipes[0]
         path.append(next)
+
+    # connections = get_connecting_pipes(flattened_map, start, nCols)
+    # next = connections[0]
+    # prev = start
+    # path = [start,next]
+    # while flattened_map[next] != 'S':
+    #     connections = get_connecting_pipes(flattened_map, next, nCols)
+    #     connections.pop(connections.index(prev))
+    #     #sanity check
+    #     if len(connections) != 1:
+    #         raise ValueError(f"At this point connections should have length of 1 but does not. {connections}")
+    #     prev = next
+    #     next = connections[0]
+    #     path.append(next)
     
-    import pdb;pdb.set_trace()
-    farthest_steps = len(path)//2 + len(path)%2
+    farthest_steps = len(path)//2
     print(f"The number of steps farthest from the starting position is {farthest_steps}")
+    # import pdb;pdb.set_trace()
+
+def find_pipes_leading_out_start(map, this, nCols):
+    connections = []
+
+    # Check North
+    north_pos = this-nCols
+    north = map[north_pos]
+    if north in LEADING_NORTH:
+        connections.append(north_pos)
+
+    # Check East
+    east_pos = this-1
+    east = map[east_pos]
+    if east in LEADING_EAST:
+        connections.append(east_pos)
+
+    # Check West
+    west_pos = this+1
+    west = map[west_pos]
+    if west in LEADING_WEST:
+        connections.append(west_pos)
+    
+    # Check South
+    south_pos = this+nCols
+    south = map[south_pos]
+    if south in LEADING_SOUTH:
+        connections.append(south_pos)
+
+    if len(connections) != 2:
+        raise ValueError(f"Number of connection leading out of start should be two but was {len(connections)} instead.")
+    
+    return connections
 
 def get_connecting_pipes(map, this, nCols):
     connections = []

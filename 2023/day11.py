6dd2e702f451e307a442b7934e44612a62712e75
Ed Manlove <emanlove@verizon.net>
2023-12-28 11:29:21 -0500

    Initial solution for part 2 day 11
    Checking I see I am off a little .. thinking it may be a off by one
    multiplier in the shift .. checking now ..

--- 2023/day11.py
+++ 2023/day11.py
@@ -1,7 +1,7 @@
 import sys
 import itertools
 
-def read_skydata(filename):
+def read_skydata(filename,scale):
     with open(filename,'r') as fh:
         lines = [line.rstrip('\n') for line in fh]
 
@@ -28,8 +28,8 @@ def read_skydata(filename):
                 abs_indx = cindx + (rindx*nCols)
                 # noting that abs_indx is on the NON-EXPANDED universe cordinates 
                 # that's ok but maybe we want it in the expanded unirvrse cordinates ..
-                r = rindx + shift_by(empty_rows,rindx)
-                c = cindx + shift_by(empty_cols,cindx)
+                r = rindx + shift_by(empty_rows,rindx,scale)
+                c = cindx + shift_by(empty_cols,cindx,scale)
                 galaxies[abs_indx] = (r,c)
     
     uniq_pairs = list(itertools.combinations(galaxies.keys(), 2))
@@ -41,12 +41,16 @@ def read_skydata(filename):
         dist = abs(beta[1]-alpha[1]) + abs(beta[0]-alpha[0])
         distances.append(dist)
 
-    print(f"The sum of the lengths of the shortest path between every pair of galaxies is {sum(distances)}")
+    print(f"The sum of the lengths of the shortest path between every pair of galaxies"
+          f" in a scale of {scale} is {sum(distances)}")
 
-def shift_by(empties, indx):
-    return sum([1 for e in empties if indx > e])
+def shift_by(empties, indx, scale):
+    return sum([1 for e in empties if indx > e])*scale
 
 if __name__ == "__main__":
     file = sys.argv[1]
-
-    read_skydata(file)
+    if len(sys.argv)==3:
+        scale = int(sys.argv[2])
+    else:
+        scale = 1
+    read_skydata(file,scale)

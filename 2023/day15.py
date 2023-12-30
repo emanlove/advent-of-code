1121b369a3f10719e3aee45bf0d297ff2d2a2578
Ed Manlove <emanlove@verizon.net>
2023-12-30 16:20:15 -0500

    Solution for Part 2 Day 15

--- 2023/day15.py
+++ 2023/day15.py
@@ -1,6 +1,6 @@
 import sys
 
-def read_initialization_sequence(filename):
+def blindly_read_initialization_sequence(filename):
     with open(filename,'r') as fh:
         lines = [line.rstrip('\n') for line in fh]
 
@@ -21,6 +21,46 @@ def run_HASH_algorithm(string):
         current %= 256
 
     return current
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+
+    blindly_read_initialization_sequence(file)
+
+import re
+from collections import OrderedDict
+
+REMOVE = '-'
+INSERT = '='
+
+def read_initialization_sequence(filename):
+    with open(filename,'r') as fh:
+        lines = [line.rstrip('\n') for line in fh]
+
+    steps = lines[0].split(',')
+
+    boxes = {n:OrderedDict() for n in range(256)}
+    for step in steps:
+        hashed_box, focal_length_str = re.split('[-=]',step)
+        operation = step[len(hashed_box)]   # might be able to get the matching char out of re
+        box = run_HASH_algorithm(hashed_box)
+
+        if operation == REMOVE:
+            if hashed_box in boxes[box]:
+                del boxes[box][hashed_box]
+        elif operation == INSERT:
+            focal_length = int(focal_length_str)
+            boxes[box][hashed_box] = focal_length
+        else:
+            print(f"!!WARNING!! unknown operation - {operation}")
+
+    focusing_power = []
+    for zero_box_indx, box in enumerate(boxes):
+        for zero_lense_indx, lense in enumerate(boxes[box]):
+            lense_power = (zero_box_indx+1)*(zero_lense_indx+1)*boxes[box][lense]
+            focusing_power.append(lense_power)
+    
+    print(f"The total focusing power of the resulting lens configuration is {sum(focusing_power)}")
 
 if __name__ == "__main__":
     file = sys.argv[1]

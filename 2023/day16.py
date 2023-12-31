ad0a04988c08e360a7fffa8a8360fc88185bbbc2
Ed Manlove <emanlove@verizon.net>
2023-12-30 20:44:14 -0500

    Fleshed out and coded a bit more design
    In writing out the initial path to the first reflector I saw this would
    be the same from any path. So made this a function, `find_next_reflector`,
    which traces out a path and depending on the direction which reflectors
    to ignore. (That is the pointy ends of splitters). Then it looks down
    that path to find the next reflector. If no reflector is found it will
    return None as the next reflectore. It also returns the tiles it
    traversed.
    
    Next we will need to react to a beam hitting a reflector ..

--- 2023/day16.py
+++ 2023/day16.py
@@ -1,9 +1,11 @@
 import sys
 
+type  = 'type'
 left  = 'left'
 right = 'right'
 up    = 'up'
 down  = 'down'
+beams = 'beams'
 
 def read_contraption (filename):
     with open(filename,'r') as fh:
@@ -11,26 +13,60 @@ def read_contraption (filename):
     
     flattened = ''.join(lines)
 
-    reflectors = {}
-    for coord,point in enumerate(flattened):
-        if point != '.':
-            reflectors[point] = {}
-            reflectors[point][left] = False
-            reflectors[point][right] = False
-            reflectors[point][up] = False
-            reflectors[point][down] = False
+    ncols = len(lines[0])
+    nrows = len(lines)
 
+    reflectors = {}
+    for coord,tile in enumerate(flattened):
+        if tile != '.':
+            reflectors[coord] = {}
+            reflectors[coord][type] = tile
+            reflectors[coord][left] = False
+            reflectors[coord][right] = False
+            reflectors[coord][up] = False
+            reflectors[coord][down] = False
+            reflectors[coord][beams] = []
     return reflectors
+
+def find_next_reflector(reflectors, pos, heading):
+    match heading:
+        case [left]:
+            pass
+            # path = []  # indices of points to the left
+            # ignore = ['-']
+        case [right]:
+            pass
+            # path = []  # indices of points to the right
+            # ignore = ['-']
+        case [up]:
+            pass
+            # path = []  # indices of points upward
+            # ignore = ['|']
+        case [down]:
+            pass
+            # path = []  # indices of points downward
+            # ignore = ['|']
+
+    for step in path:
+        if (step in reflectors) and reflectors[step][type] not in ignore:
+            traversed = ...
+            next_reflector = step
+            return next_reflector, traversed  # return next reflector or None if not one AND the tiles traversed            
+    traversed = path
+    next_reflector = None
+    return next_reflector, traversed  # return next reflector or None if not one AND the tiles traversed
 
 def shine_light(reflectors):
 
-    beams = []
-    initial_beam = {pos: 0, heading: right}
-    beams.append(initial_beam)
+    # trace to initial reflector
+    path = range(ncols)
 
-    while beams:
-        beams=[]
-    
+    # while any beams
+    while any(reflectors[pnt][beams] for pnt in reflectors):
+        pass
+
+    # count up number of tiles
+
     print(f"The number of tiles end up being energized is ")
 
 

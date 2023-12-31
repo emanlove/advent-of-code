565baba1866162f2d8b4aa93ee16dd4be29766f9
Ed Manlove <emanlove@verizon.net>
2023-12-31 11:09:38 -0500

    Shifted from a method based solution to a class based solution
    Also added methods for adding and removing a beam from a reflector.

--- 2023/day16.py
+++ 2023/day16.py
@@ -1,160 +1,196 @@
 import sys
 
-# type  = 'type'
-# left  = 'left'
-# right = 'right'
-# up    = 'up'
-# down  = 'down'
-# beams = 'beams'
 TYPE  = 'type'
 LEFT  = 'left'
 RIGHT = 'right'
 UP    = 'up'
 DOWN  = 'down'
 BEAMS = 'beams'
-NCOLS = None
-NROWS = None
 
-def read_contraption (filename):
-    with open(filename,'r') as fh:
-        lines = [line.rstrip('\n') for line in fh]
+class Contraption():
+    def __init__(self) -> None:
+        self.reflectors = {}
+        self.ncols = None
+        self.nrows = None
+        self.all_tiles_energized = []
+
+    def read_contraption (self, filename):
+        with open(filename,'r') as fh:
+            lines = [line.rstrip('\n') for line in fh]
     
-    flattened = ''.join(lines)
+        flattened = ''.join(lines)
 
-    ncols = len(lines[0])
-    nrows = len(lines)
-    NCOLS = len(lines[0])
-    NROWS = len(lines)
+        self.ncols = len(lines[0])
+        self.nrows = len(lines)
 
-    reflectors = {}
-    for coord,tile in enumerate(flattened):
-        if tile != '.':
-            reflectors[coord] = {}
-            reflectors[coord][TYPE] = tile
-            reflectors[coord][LEFT] = False
-            reflectors[coord][RIGHT] = False
-            reflectors[coord][UP] = False
-            reflectors[coord][DOWN] = False
-            reflectors[coord][BEAMS] = []
-    return reflectors
+        for coord,tile in enumerate(flattened):
+            if tile != '.':
+                self.reflectors[coord] = {}
+                self.reflectors[coord][TYPE] = tile
+                self.reflectors[coord][LEFT] = False
+                self.reflectors[coord][RIGHT] = False
+                self.reflectors[coord][UP] = False
+                self.reflectors[coord][DOWN] = False
+                self.reflectors[coord][BEAMS] = []
 
-def find_next_reflector(reflectors, pos, heading):
-    match heading:
-        case "left":
-            # indices of points to the left
-            # range(pos, beginning of this row-1, -1)
-            # range(pos-1, (pos//NCOLS)*NCOLS)-1, -1)
-            path = range(pos-1, ((pos//NCOLS)*NCOLS)-1, -1)
-            ignore = ['-']
-        case "right":
-            # indices of points to the right
-            # range(pos+1, (pos//NCOLS+1)*NCOLS), +1)
-            path = range(pos+1, (pos//NCOLS+1)*NCOLS, +1)  # indices of points to the right
-            ignore = ['-']
-        case "up":
-            # indices of points upward
-            # range(row above, 0, -num of cols)
-            # range(pos-NCOLS, 0, -NCOLS)
-            path = range(pos-NCOLS, 0, -NCOLS)  # indices of points upward
-            ignore = ['|']
-        case "down":
-            # indices of points downward
-            # range(row below, num of cols, +num of cols)
-            # range(pos+NCOLS, NCOLS, +NCOLS)
-            path = range(pos+NCOLS, NCOLS, +NCOLS)  # indices of points downward
-            ignore = ['|']
+    def find_next_reflector(self, pos, heading):
+        reflectors = self.reflectors
+        ncols = self.ncols
+        nrows = self.nrows
 
-    for indx,step in enumerate(path):
-        if (step in reflectors) and reflectors[step][type] not in ignore:
-            traversed = path[:indx+1]
-            next_reflector = step
-            # ?? remove beam from reflector (conditional based upon pos in reflector - needed for initial start)
-            # remove beam from reflector
-            if pos in reflectors:
-                beam_indx = reflectors[pos][BEAMS].find(heading)
-                reflectors[pos][BEAMS].pop(beam_indx)
-            return next_reflector, traversed  # return next reflector or None if not one AND the tiles traversed            
-    traversed = path
-    next_reflector = None
-    # ?? remove beam from reflector (conditional based upon pos in reflector - needed for initial start)
-    # remove beam from reflector
-    if pos in reflectors:
-        beam_indx = reflectors[pos][BEAMS].find(heading)
-        reflectors[pos][BEAMS].pop(beam_indx)
-    return next_reflector, traversed  # return next reflector or None if not one AND the tiles traversed
+        match heading:
+            case "left":
+                # indices of points to the left
+                # range(pos, beginning of this row-1, -1)
+                # range(pos-1, (pos//NCOLS)*NCOLS)-1, -1)
+                path = range(pos-1, ((pos//ncols)*ncols)-1, -1)
+                ignore = ['-']
+            case "right":
+                # indices of points to the right
+                # range(pos+1, (pos//NCOLS+1)*NCOLS), +1)
+                path = range(pos+1, (pos//ncols+1)*ncols, +1)  # indices of points to the right
+                ignore = ['-']
+            case "up":
+                # indices of points upward
+                # range(row above, 0, -num of cols)
+                # range(pos-NCOLS, 0, -NCOLS)
+                path = range(pos-ncols, 0, -ncols)  # indices of points upward
+                ignore = ['|']
+            case "down":
+                # indices of points downward
+                # range(row below, num of rows, +num of cols)
+                # range(pos+NCOLS, NROWS, +NCOLS)
+                path = range(pos+ncols, nrows, +ncols)  # indices of points downward
+                ignore = ['|']
 
-def reflect_beam(reflectors, pos, coming_from):
-    type = reflectors[pos][TYPE]
+        for indx,step in enumerate(path):
+            if (step in reflectors) and reflectors[step][type] not in ignore:
+                traversed = path[:indx+1]
+                next_reflector = step
+                self.remove_beam_from_reflector(pos, heading)
 
-    # if beam coming into this reflector has arrived from same direction before
-    # then don't reflect back out (as this would be looping the light)
-    
-    match type:
-        case '|':
-            match coming_from:
-                case "left" | "right":
-                    # split up and down
-                    pass
-                case _:
-                    print(f"!!WARNING!! Should not be approching | ({pos}) from {coming_from}")
-        case '-':
-            match coming_from:
-                case "up" | "down":
-                    # split left and right
-                    pass
-                case _:
-                    print(f"!!WARNING!! Should not be approching - ({pos}) from {coming_from}")
-        case '\':
-            match coming_from:
-                case "left":
-                    # reflect up
-                    pass
-                case "right":
-                    # reflect down
-                    pass
-                case "up":
-                    # reflect left
-                    pass
-                case "down":
-                    # reflect right
-                    pass
-        case '/':
-            match coming_from:
-                case "left":
-                    # reflect up
-                    pass
-                case "right":
-                    # reflect down
-                    pass
-                case "up":
-                    # reflect right
-                    pass
-                case "down":
-                    # reflect left
-                    pass
+                # return next reflector
+                return next_reflector, traversed
 
-    # ?? How and when/where do we remove a beam 
+        # return next reflector as None since there is not one
+        traversed = path
+        next_reflector = None
+        self.remove_beam_from_reflector(pos, heading)
 
-def shine_light(reflectors):
+        return next_reflector, traversed
 
-    all_tiles_energized = [0]
+    def remove_beam_from_reflector(self, indx, heading):
+        # remove beam from reflector (conditional based upon pos in reflector - needed for initial start)
+        reflectors = self.reflectors
 
-    # trace to initial reflector
-    next_reflector, tiles_traversed = find_next_reflector(reflectors, 0,RIGHT)
-    all_tiles_energized.append(tiles_traversed)
-    reflect_beam(reflectors, next_reflector, RIGHT)
+        if indx in reflectors:
+            beam_indx = reflectors[indx][BEAMS].find(heading)
+            reflectors[indx][BEAMS].pop(beam_indx)
 
-    # while any beams
-    while any(reflectors[pnt][BEAMS] for pnt in reflectors):
-        pass
 
-    # count up number of tiles
+    def reflect_beam(self, pos, coming_from):
+        reflectors = self.reflectors
 
-    print(f"The number of tiles end up being energized is ")
+        # if beam coming into this reflector has arrived from same direction before
+        # then don't reflect back out (as this would be looping the light)
+        if reflectors[pos][coming_from]:
+            return
+
+        # note incoming beam so we don't repeat in future
+        self.record_beam(pos, coming_from)
+
+        type = reflectors[pos][TYPE]
+        match type:
+            case '|':
+                match coming_from:
+                    case "left" | "right":
+                        # split up and down
+                        self.add_beam(pos, UP)
+                        self.add_beam(pos, DOWN)
+                    case _:
+                        print(f"!!WARNING!! Should not be approching | ({pos}) from {coming_from}")
+            case '-':
+                match coming_from:
+                    case "up" | "down":
+                        # split left and right
+                        self.add_beam(pos, LEFT)
+                        self.add_beam(pos, RIGHT)
+                    case _:
+                        print(f"!!WARNING!! Should not be approching - ({pos}) from {coming_from}")
+            case '\':
+                match coming_from:
+                    case "left":
+                        # reflect up
+                        self.add_beam(pos, UP)
+                    case "right":
+                        # reflect down
+                        self.add_beam(pos, DOWN)
+                    case "up":
+                        # reflect left
+                        self.add_beam(pos, LEFT)
+                    case "down":
+                        # reflect right
+                        self.add_beam(pos, RIGHT)
+            case '/':
+                match coming_from:
+                    case "left":
+                        # reflect up
+                        self.add_beam(pos, UP)
+                    case "right":
+                        # reflect down
+                        self.add_beam(pos, DOWN)
+                    case "up":
+                        # reflect right
+                        self.add_beam(pos, RIGHT)
+                    case "down":
+                        # reflect left
+                        self.add_beam(pos, LEFT)
+
+    def add_beam(self, pos, going):
+        reflectors = self.reflectors
+
+        if going in reflectors[pos][BEAMS]:
+            print(f"Note: Finding duplicate beams")            
+            return
+        
+        reflectors[pos][BEAMS].append(going)
+
+    def record_beam(self, pos, heading):
+        reflectors = self.reflectors
+
+        if reflectors[pos][heading]:
+            print(f"!!WARNING!! Should have caught previously record beam. pos:{pos} heading:{heading}")
+        
+        reflectors[pos][heading] = True
+
+    def shine_light(self):
+        reflectors = self.reflectors
+        self.all_tiles_energized = [0]
+
+        # trace to initial reflector
+        next_reflector, tiles_traversed = self.find_next_reflector(0, RIGHT)
+        self.all_tiles_energized.append(tiles_traversed)
+        if next_reflector:
+            reflect_beam(next_reflector, RIGHT)
+
+        # while any beams
+        while any(reflectors[pnt][BEAMS] for pnt in reflectors):
+            for reflector in reflectors:
+                for heading in reflector[BEAMS]:
+                    next_reflector, tiles_traversed = self.find_next_reflector(reflector, heading)
+                    self.all_tiles_energized.append(tiles_traversed)
+                    if next_reflector:
+                        reflect_beam(next_reflector, heading)
+
+        # count up number of tiles
+        tiles_energized = len(set(self.all_tiles_energized))
+
+        print(f"The number of tiles end up being energized is {tiles_energized}")
 
 
 if __name__ == "__main__":
     file = sys.argv[1]
 
-    reflectors = read_contraption(file)
-    shine_light(reflectors)
+    ctrap = Contraption()
+    ctrap.read_contraption(file)
+    ctrap.shine_light()

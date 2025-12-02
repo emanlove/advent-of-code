09a9615d3d49cc2197d7fc6e5e8ddc784d25aa33
Ed Manlove <emanlove@verizon.net>
2025-12-02 15:38:01 -0500

    Solution to Day 2 Part 2

--- 2025/day2.py
+++ 2025/day2.py
@@ -47,37 +47,8 @@ def part_2(filename):
             lower_bound_digits = lower_digits if int(str(lower_digits) * num_seqs) >= first else lower_digits + 1
             upper_bound_digits = upper_digits if int(str(upper_digits) * num_seqs) <= last else upper_digits - 1
             for digit in range(lower_bound_digits, upper_bound_digits + 1):
-                print(f"{int(str(digit) * num_seqs)}")
+                # print(f"{int(str(digit) * num_seqs)}")
                 invalid_ids.append(int(str(digit) * num_seqs))
-            # match factor:
-            #     case 1:
-            #         lower_digit = int(first_str[0]); upper_digit = int(last_str[0])
-            #         lower_bound_digit = lower_digit if int(first_str[0]*len_pair)>first else lower_digit+1
-            #         upper_bound_digit = upper_digit if int(last_str[0]*len_pair)<last else upper_digit-1
-            #         for digit in range(lower_bound_digit,upper_bound_digit+1):
-            #             # print(f"{int(str(digit)*len_pair)}")
-            #             invalid_ids.append(int(str(digit)*len_pair))
-            #     case 2:
-            #         print(f"{pair}")
-            #         lower_double = int(first_str[:2]); upper_double = int(last_str[:2])
-            #         print(f"lower: {lower_double}  upper: {upper_double}")
-            #         # lower_double = first_str[:1]; upper_double = last_str[:1]
-            #         num_pairs = len_pair // 2
-            #         # lower_bound_double = lower_double if int(lower_double*num_pairs)>first else str(int(lower_double)+1)
-            #         # upper_bound_double = upper_double if int(upper_double*num_pairs)<last else str(int(upper_double)-1)
-            #         lower_bound_double = lower_double if int(str(lower_double)*num_pairs)>first else lower_double+1
-            #         upper_bound_double = upper_double if int(str(upper_double)*num_pairs)<last else upper_double-1
-            #         for digit in range(lower_bound_double, upper_bound_double+1):
-            #             print(f"{int(str(digit)*num_pairs)}")
-            #             invalid_ids.append(int(str(digit)*num_pairs))
-            #     case 3:
-            #         pass
-            #     case 4:
-            #         pass
-            #     case 5:
-            #         pass
-            #     case _:
-            #         print('Unhandled factor')
 
     invalid_ids = list(set(invalid_ids))
     return sum(invalid_ids)
@@ -135,6 +106,5 @@ if __name__ == "__main__":
     file = sys.argv[1]
 
     # sum_invalid_ids = read_ids(file)
-    # print(f"The sum of all invalid ids is {sum_invalid_ids}")
     sum_invalid_ids  = part_2(file)
     print(f"The sum of all invalid ids is {sum_invalid_ids}")

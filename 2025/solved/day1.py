7529d47205e1e8aa938ea50a948be9648e80dc34
Ed Manlove <emanlove@verizon.net>
2025-12-01 20:08:03 -0500

    Still not right ..

--- 2025/solved/day1.py
+++ 2025/solved/day1.py
@@ -1,5 +1,32 @@
 import sys
 
+
+def read_rotations_part2(filename):
+    with open(filename, 'r') as fh:
+        lines = [line.rstrip('\n') for line in fh]
+
+    num_zeros = 0
+    this = 50
+    for line in lines:
+        direction = line[0]
+        adjustment = int(line[1:])
+
+        num_zeros += adjustment // 100
+        adjustment = adjustment % 100
+
+        if direction == 'L':
+            next = this - adjustment
+        elif direction == 'R':
+            next = this + adjustment
+
+        if (next < 1) or (next > 99):
+            num_zeros += 1
+            this = next % 100
+        else:
+            this = next
+
+    return num_zeros
+
 def read_rotations(filename):
     with open(filename, 'r') as fh:
         lines = [line.rstrip('\n') for line in fh]
@@ -11,6 +38,7 @@ def read_rotations(filename):
         adjustment = int(line[1:])
 
         if adjustment == 0:
+            print("Adjustment is zero")
             continue
 
         if direction == 'L':
@@ -26,10 +54,10 @@ def read_rotations(filename):
                 if pointer != 0:
                     num_zeros += 1
                 num_zeros += adjustment // 100
-            print(f"{pointer} -{adjustment} {num_zeros}")
+            # print(f"{pointer} -{adjustment} {num_zeros}")
             pointer -= (adjustment % 100)
             pointer = pointer % 100
-            print(f"{pointer}")
+            # print(f"{pointer}")
 
         elif direction == 'R':
             # if (100 - pointer) > adjustment:
@@ -43,10 +71,13 @@ def read_rotations(filename):
             if (100 - pointer) <= (adjustment % 100):
                 num_zeros += 1
                 num_zeros += adjustment // 100
-            print(f"{pointer} +{adjustment} {num_zeros}")
-            pointer += (adjustment % 100)
-            pointer = pointer % 100
-            print(f"{pointer}")
+            # print(f"{pointer} +{adjustment} {num_zeros}")
+            # pointer += (adjustment % 100)
+            # pointer = pointer % 100
+            pointer = (pointer + adjustment) % 100
+            if pointer == 0:
+                num_zeros += 1
+            # print(f"{pointer}")
 
         else:
             print(f"unknown direction {direction}")
@@ -77,5 +108,6 @@ def read_rotations(filename):
 if __name__ == "__main__":
     file = sys.argv[1]
 
-    zeros = read_rotations(file)
+    # zeros = read_rotations(file)
+    zeros = read_rotations_part2(file)
     print(f"The number of zeros is {zeros}")

a75282783f21df584d3a9acfc8b6f33ae253fefd
Ed Manlove <emanlove@verizon.net>
2025-12-03 11:35:34 -0500

    Solution Part 2 Day 3
    Allow for any size of batteries needed per bank

--- 2025/day3.py
+++ 2025/day3.py
@@ -1,25 +1,31 @@
 import sys
 
 
-def read_battery_banks(filename):
+def read_battery_banks(filename, num_batteries):
+
+    battery_index_limit = num_batteries - 1
     with open(filename, 'r') as fh:
         lines = [line.rstrip('\n') for line in fh]
 
     output_joltages = []
     for line in lines:
-        batteries = [int(b) for b in line]
-        leading_battery_max = max(batteries[:-1])
-        max_leading_battery_index = batteries[:-1].index(leading_battery_max)
-        following_battery_max = max(batteries[max_leading_battery_index+1:])
-        largest_possible_joltage = int(str(leading_battery_max)+str(following_battery_max))
+        battery_bank = [int(b) for b in line]
+        start_indx = 0
+        len_bank = len(battery_bank)
+        joltages_turned_on = []
+        for end_indx in range(num_batteries,0,-1):
+            battery_max = max(battery_bank[start_indx:len_bank-end_indx+1])
+            start_indx = start_indx + battery_bank[start_indx:len_bank-end_indx+1].index(battery_max) + 1
+            joltages_turned_on.append(str(battery_max))
+        largest_possible_joltage = int(''.join(joltages_turned_on))
 
         output_joltages.append(largest_possible_joltage)
 
     return sum(output_joltages)
 
-
 if __name__ == "__main__":
     file = sys.argv[1]
+    num_batteries = int(sys.argv[2])
 
-    total_output_joltage = read_battery_banks(file)
+    total_output_joltage = read_battery_banks(file, num_batteries)
     print(f"The total output joltage is {total_output_joltage}")

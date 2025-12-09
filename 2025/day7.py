b5c73d7795761563648cb66a5aa958e9a053cc12
Ed Manlove <emanlove@verizon.net>
2025-12-08 21:04:33 -0500

    Eh ..

--- 2025/day7.py
+++ 2025/day7.py
@@ -27,42 +27,67 @@ def calculate_num_splits(S, untouched_reflectors, tilted_diagram):
 
     num_splits = 0
     # while(started_beams):
-    debug_while = 25
+    debug_while = 50
     while debug_while:
+        # print(f"{started_beams}")
+        add_beams = []
+        remove_beams = []
         for beam in started_beams:
             # print(f"{beam}")
-            added_beams = []
-            removed_beams = []
             # from starting point (beam) check for any reflectors in remaining
             # part of the row
-            if '^' not in tilted_diagram[beam[0]][beam[1]:]:
-                # todo: might save how far beam went
-                # remove this beam
-                removed_beams.append(beam)
-                # indx = started_beams.index(beam)
-                # started_beams.pop(indx)
-            else:
-                num_splits += 1
+            if '^' in tilted_diagram[beam[0]][beam[1]:]:
                 next_reflector = (beam[0], tilted_diagram[beam[0]][beam[1]:].index('^')+beam[1])
-                print(f"{next_reflector}")
-                print(f"{untouched_reflectors}")
-                ref_index = untouched_reflectors.index(next_reflector)
-                untouched_reflectors.pop(ref_index)
+                if next_reflector not in untouched_reflectors:
+                    next_reflector = None
+
+            if next_reflector:
+                # print(f"{next_reflector}")
+                # print(f"{untouched_reflectors}")
+
+                num_splits += 1
+                if next_reflector in untouched_reflectors:
+                    ref_index = untouched_reflectors.index(next_reflector)
+                    untouched_reflectors.pop(ref_index)
+                else:
+                    # num_splits -= 1
+                    print("?? - should not have this ..")
                 new_beams = [(next_reflector[0]-1,next_reflector[1]), (next_reflector[0]+1,next_reflector[1])]
-                removed_beams.append(beam)
                 for new_beam in new_beams:
-
                     if new_beam not in started_beams:
-                        added_beams.append(new_beam)
+                        add_beams.append(new_beam)
+            remove_beams.append(beam)
 
-        for remove in removed_beams:
+
+            # if '^' not in tilted_diagram[beam[0]][beam[1]:]:
+            #     # todo: might save how far beam went
+            #     # remove this beam
+            #     removed_beams.append(beam)
+            #     # indx = started_beams.index(beam)
+            #     # started_beams.pop(indx)
+            # else:
+            #     num_splits += 1
+            #     next_reflector = (beam[0], tilted_diagram[beam[0]][beam[1]:].index('^')+beam[1])
+            #     print(f"{next_reflector}")
+            #     print(f"{untouched_reflectors}")
+            #     ref_index = untouched_reflectors.index(next_reflector)
+            #     untouched_reflectors.pop(ref_index)
+            #     new_beams = [(next_reflector[0]-1,next_reflector[1]), (next_reflector[0]+1,next_reflector[1])]
+            #     removed_beams.append(beam)
+            #     for new_beam in new_beams:
+            #
+            #         if new_beam not in started_beams:
+            #             added_beams.append(new_beam)
+
+        for remove in remove_beams:
             # import pdb;pdb.set_trace()
             try:
                 indx = started_beams.index(remove)
                 started_beams.pop(indx)
             except ValueError:
-                raise Exception(f"Unable to find beam {remove} that needs to be removed.\n{started_beams}")
-        for added in added_beams:
+                pass # for now
+                # raise Exception(f"Unable to find beam {remove} that needs to be removed.\n{started_beams}")
+        for added in add_beams:
             started_beams.append(added)
         debug_while -= 1
 
@@ -72,6 +97,6 @@ if __name__ == "__main__":
     file = sys.argv[1]
 
     S, untouched_reflectors, tilted_diagram = read_diagram_and_tilt(file)
-    print(f"{S}\n{untouched_reflectors}\n{tilted_diagram}")
+    # print(f"{S}\n{untouched_reflectors}\n{tilted_diagram}")
     num_splits = calculate_num_splits(S, untouched_reflectors, tilted_diagram)
     print(f"The number of times the beam will split is {num_splits}")

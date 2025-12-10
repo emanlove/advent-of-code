af45d293bf77ff9be5cb211057a4d0851c322c45
Ed Manlove <emanlove@verizon.net>
2025-12-09 20:37:13 -0500

    Still have a misunderstanding about whether a box is pair and what is added or skipped ...

--- 2025/day8.py
+++ 2025/day8.py
@@ -12,6 +12,7 @@ def read_junction_boxes(filename, n_closest_boxes):
         junction_boxes.append((int(x), int(y), int(z)))
 
     num_junction_boxes = len(junction_boxes)
+    print(f"Number of junction boxes {num_junction_boxes}")
 
     distance_matrix = []
     for pointA_index, pointA in enumerate(junction_boxes):
@@ -27,8 +28,19 @@ def read_junction_boxes(filename, n_closest_boxes):
     circuits = [{sorted_distance_matrix[0][1],sorted_distance_matrix[0][2]}]
 
     # for dist, boxA, boxB in sorted_distance_matrix[1:]:
+    # for count, (dist, boxA, boxB) in enumerate(sorted_distance_matrix[1:n_closest_boxes]):
+    # keep track of un/connected boxes
+    unconnected_boxes = set(range(num_junction_boxes))
+    # remove the initial two boxes
+    unconnected_boxes.remove(sorted_distance_matrix[0][1])
+    unconnected_boxes.remove(sorted_distance_matrix[0][2])
     for dist, boxA, boxB in sorted_distance_matrix[1:n_closest_boxes]:
         new_pair = {boxA, boxB}
+        # print(f"{unconnected_boxes}: {new_pair}")
+        # if not unconnected_boxes.isdisjoint(new_pair):
+        if new_pair - unconnected_boxes:
+            # print("Pair has at least one connected box")
+            continue
         found_circuit = False
         for circuit in circuits:
             if not circuit.isdisjoint(new_pair):
@@ -38,6 +50,10 @@ def read_junction_boxes(filename, n_closest_boxes):
 
         if not found_circuit:
             circuits.append(new_pair)
+
+        # remove these now connected boxes
+        unconnected_boxes.remove(boxA)
+        unconnected_boxes.remove(boxB)
 
     return circuits
 
@@ -51,7 +67,8 @@ if __name__ == "__main__":
 
     circuits = read_junction_boxes(file, n_closest_boxes)
     len_circuits = [len(c) for c in circuits]
+    print(f"The sizes of each circuit are {len_circuits}")
     top_three_circuit_lengths = sorted(len_circuits, reverse=True)[:3]
-    print(f"The the three largest circuits are {top_three_circuit_lengths}")
+    print(f"The three largest circuits are {top_three_circuit_lengths}")
     product_of_top_three_circuits = math.prod(top_three_circuit_lengths)
     print(f"The product of the three largest circuits sizes is {product_of_top_three_circuits}")
